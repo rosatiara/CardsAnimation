7969ed6fa1e6a6ead6ea908501feb19316ba8934
rosa <rosatiaragaluh@gmail.com>
2021-12-18 13:57:56 +0700

    Initial Commit

---
+++ Shared/CardsAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsAnimationApp.swift
+//  Shared
+//
+//  Created by T on 18/12/21.
+//
+
+import SwiftUI
+
+@main
+struct CardsAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
